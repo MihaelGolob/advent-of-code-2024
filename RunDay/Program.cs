4d0dbd55d093187dab30df3f7927723c2896cd5c
mihael.golob <mihael.golob@outfit7.com>
2024-12-03 06:56:09 +0100

    add timing

--- RunDay/Program.cs
+++ RunDay/Program.cs
@@ -1,6 +1,12 @@
 ﻿var day = new Day03.Day03();
-var result1 = day.RunPart1();
-var result2 = day.RunPart2();
 
-Console.WriteLine($"Part1 result is: {result1}");
-Console.WriteLine($"Part2 result is: {result2}");
+var watch1 = System.Diagnostics.Stopwatch.StartNew();
+var result1 = day.RunPart1();
+watch1.Stop();
+
+var watch2 = System.Diagnostics.Stopwatch.StartNew();
+var result2 = day.RunPart2();
+watch2.Stop();
+
+Console.WriteLine($"Part1 result is: {result1} ({watch1.ElapsedMilliseconds}ms)");
+Console.WriteLine($"Part2 result is: {result2} ({watch2.ElapsedMilliseconds}ms)");
