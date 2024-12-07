7053eae09b74f76efe66a5d02523435a1151c80e
mihael.golob <mihael.golob@outfit7.com>
2024-12-07 07:39:42 +0100

    prepare for day 8

---
+++ Day08/Day08.cs
@@ -0,0 +1,19 @@
+﻿namespace Day08;
+
+public class Day08 : DayBase
+{
+    protected override int DayIndex()
+    {
+        return 8;
+    }
+
+    public override string RunPart1()
+    {
+        return "";
+    }
+
+    public override string RunPart2()
+    {
+        return "";
+    }
+}
