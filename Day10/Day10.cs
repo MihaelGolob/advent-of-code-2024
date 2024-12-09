04d6276491a171f1a0b48fb26230fb69c5ede085
mihael.golob <mihael.golob@outfit7.com>
2024-12-09 07:48:34 +0100

    prepare for day 10

---
+++ Day10/Day10.cs
@@ -0,0 +1,19 @@
+﻿namespace Day10;
+
+public class Day10 : DayBase
+{
+    protected override int DayIndex()
+    {
+        return 10;
+    }
+
+    public override string RunPart1()
+    {
+        return "";
+    }
+
+    public override string RunPart2()
+    {
+        return "";
+    }
+}
--- RunDay/Program.cs
+++ RunDay/Program.cs
@@ -1,4 +1,4 @@
-﻿var day = new Day09.Day09(); 
+﻿var day = new Day10.Day10(); 
 
 var watch1 = System.Diagnostics.Stopwatch.StartNew();
 var result1 = day.RunPart1();
