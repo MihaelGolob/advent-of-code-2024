0bbb30b47f70dbcce2a90a08c3f0190df2f9befb
mihael.golob <mihael.golob@outfit7.com>
2024-12-11 07:43:44 +0100

    prepare for day 12

---
+++ Day12/Day12.cs
@@ -0,0 +1,15 @@
+﻿namespace Day12;
+
+public class Day12 : DayBase {
+    protected override int DayIndex() {
+        return 12;
+    }
+
+    public override string RunPart1() {
+        return "";
+    }
+
+    public override string RunPart2() {
+        return "";
+    }
+}
