7a0da1a9e529fa941a7b4e1876b607a3e478048d
mihael.golob <mihael.golob@outfit7.com>
2024-12-14 13:36:12 +0100

    prepare for day 15

---
+++ Day15/Day15.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace Day15;
+
+public class Day15 : DayBase {
+    protected override int DayIndex() {
+        return 15;
+    }
+
+    public override string RunPart1(Stopwatch stopwatch) {
+        throw new NotImplementedException();
+    }
+
+    public override string RunPart2(Stopwatch stopwatch) {
+        throw new NotImplementedException();
+    }
+}
