a6fbd12b1178617486222a7605f6e9b187dfa250
mihael.golob <mihael.golob@outfit7.com>
2024-12-18 13:42:34 +0100

    prepare day 19

---
+++ Day19/Day19.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace Day19;
+
+public class Day19 : DayBase {
+    protected override int DayIndex() {
+        return 19;
+    }
+
+    public override string RunPart1(Stopwatch stopwatch) {
+        return "";
+    }
+
+    public override string RunPart2(Stopwatch stopwatch) {
+        return "";
+    }
+}
